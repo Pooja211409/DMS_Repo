4b97099189748ad5931912a46bfd81de43dfbbe2
poojamkokane <poojamkokane@gmail.com>
2024-07-05 15:38:37 +0530

    eureka server added without configuration

---
+++ AmulMilkProductEurekaServer/src/main/java/com/amul/server/main/AmulMilkProductEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.amul.server.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmulMilkProductEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmulMilkProductEurekaServerApplication.class, args);
+	}
+
+}
---
+++ AmulMilkProductEurekaServer/src/test/java/com/amul/server/main/AmulMilkProductEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amul.server.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmulMilkProductEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
