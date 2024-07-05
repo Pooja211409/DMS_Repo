fd167409c5d021c31c276d9e30bbe9ecf44e63ad
poojamkokane <poojamkokane@gmail.com>
2024-07-05 15:47:46 +0530

    FarmerService Added

---
+++ AmulFarmerService/src/main/java/com/amul/farmer/main/AmulFarmerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.amul.farmer.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmulFarmerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmulFarmerServiceApplication.class, args);
+	}
+
+}
---
+++ AmulFarmerService/src/test/java/com/amul/farmer/main/AmulFarmerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amul.farmer.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmulFarmerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
